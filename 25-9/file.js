3131c68366714f827f1cb30b3a8e7be2942aca54
CMITS002 <146437239+CMITS002@users.noreply.github.com>
2023-09-29 16:14:19 +0530

    Add files via upload

---
+++ 25-9/file.js
@@ -0,0 +1,7 @@
+alert('external is loading')
+
+var f_name = prompt('enter ur first name')
+var l_name = prompt('enter  ur lastname')
+
+var full_name  = f_name +  ' ' + l_name
+document.write("ur full name is.." + full_name)
